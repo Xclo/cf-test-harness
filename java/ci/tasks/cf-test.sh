92e8b00c3dbf039e59d854cf475a0897263e42f5
Rajesh Jain <rajesh.jain@pivotal.io>
2017-03-13 23:19:44 -0400

    Update Pipeline

--- java/ci/tasks/cf-test.sh
+++ java/ci/tasks/cf-test.sh
@@ -9,10 +9,12 @@ cf api $cf-api --skip-ssl-validation
 
 cf login -u $cf-username -p $cf-password -o "$cf-org" -s "$cf-space"
 
-cf apps
+export TEST_URL=`cf curl /v2/apps/$(cf app $1 --guid)/env | jq -c .application_env_json.VCAP_APPLICATION.application_uris[0]| sed "s/\"//g"`
 
 set +e
 
-
 echo "Running cf tests..."
-wget http://java-$cf-app-suffix.$cf-app-domain/$cf-test-url-endpoint
+
+echo $TEST_URL
+
+curl -k https://$TEST_URL/health
