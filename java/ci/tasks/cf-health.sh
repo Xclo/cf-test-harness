c9509ce2ab23b5d43da5392b060f620755782729
Rajesh Jain <rajesh.jain@pivotal.io>
2017-03-20 23:49:03 -0400

    Update Pipeline

---
+++ java/ci/tasks/cf-health.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+pwd
+env
+
+
+cf api $cf_api --skip-ssl-validation
+
+cf login -u $cf_username -p $cf_password -o "$cf_org" -s "$cf_space"
+
+export TEST_URL=`cf curl /v2/apps/$(cf app $cf_app --guid)/env | jq -c .application_env_json.VCAP_APPLICATION.application_uris[0]| sed "s/\"//g"`
+
+set +e
+
+echo "Running cf health tests..."
+
+echo $TEST_URL
+
+curl -k https://$TEST_URL/health
